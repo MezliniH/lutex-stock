eae353013666ad7dc0f9fb7b15eb55d6de2743be
MezliniH <gun19xx@gmail.com>
2024-03-20 12:56:37 +0100

    still working on it

--- btndata.py
+++ btndata.py
@@ -16,47 +16,50 @@ class Main(QMainWindow, FORM_CLASS):
     def handel_buttons(self):
         self.refresh.clicked.connect(self.get_data)
         self.add.clicked.connect(self.add_row)
-        # self.delete.clicked.connect(self.delete_row)  # Connect the Delete button to delete_row method
 
     def table_setup(self):
         self.tab.setColumnCount(6)
-        self.tab.setHorizontalHeaderLabels(['List', 'model', 'ref/couleur', 'rapport de coupe', 'consomation', 'stock reel'])
+        self.tab.setHorizontalHeaderLabels(['nom de list', 'model', 'ref/couleur', 'stock', 'rapport de coupe', 'consomation', 'stock_reel'])
+
+    def add_row(self):
+        row_data = [
+            self.nom_de_list.text(),
+            self.model.text(),
+            self.ref_couleur.text(),
+            float(self.stock.text()),
+            float(self.rapport_de_coupe.text()),
+            float(self.consomation.text()),
+            float(self.stock_reel.text())
+        ]
+
+        with sqlite3.connect("lutexdb") as db:
+            cursor = db.cursor()
+            command = '''INSERT INTO stock (nom de list, model, ref_couleur, stock, rapport_de_coupe, consomation, stock_reel) VALUES (?, ?, ?, ?, ?, ?, ?)'''
+            cursor.execute(command, row_data)
+            db.commit()
+
+            self.get_data()  # Refresh the table after adding new row
 
     def get_data(self):
         with sqlite3.connect("lutexdb") as db:
             cursor = db.cursor()
-            command = '''SELECT s.*, p.stock FROM stock s INNER JOIN products p ON s.ref_couleur = p.ref_couleur'''
+            command = '''SELECT * FROM stock'''
 
             result = cursor.execute(command)
             self.tab.setRowCount(0)
 
             for row_count, row_data in enumerate(result):
                 self.tab.insertRow(row_count)
-                for column_count, data in enumerate(row_data):
-                    if column_count == 5:  # Check if it's the "stock" column
-                        stock = data
-                    self.tab.setItem(row_count, column_count, QTableWidgetItem(str(data)))
-
-                consomation = float(self.tab.item(row_count, 4).text())
-                rapport_de_coupe = float(self.tab.item(row_count, 3).text())
+                stock = row_data[5]
+                consomation = row_data[4]
+                rapport_de_coupe = row_data[3]
                 stock_reel = stock - (consomation * rapport_de_coupe)
 
-                self.tab.setItem(row_count, 5, QTableWidgetItem(str(stock_reel)))
+                for column_count, data in enumerate(row_data):
+                    self.tab.setItem(row_count, column_count, QTableWidgetItem(str(data)))
 
+                self.tab.setItem(row_count, 6, QTableWidgetItem(str(stock_reel)))
 
-     
-    def add_row(self):
-        row_data = ["", "", "", "", "", ""]
-        with sqlite3.connect("lutexdb") as db:
-            cursor = db.cursor()
-            command = ''' INSERT INTO stock VALUES (?, ?, ?, ?, ?, ?) '''
-            cursor.execute(command,row_data)
-            db.commit()
-
-            self.get_data()  # Refresh the table after adding new row
-
-
-    
 
 def main():
     app = QApplication(sys.argv)
